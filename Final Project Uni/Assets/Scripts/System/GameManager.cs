3cc6257bad199b7dd95179545e4b293fb114e973
diondam <dion.dam.4802@gmail.com>
2024-09-12 22:42:11 +0700

    + dam: complete base ui, sound for UI

---
+++ Final Project Uni/Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        InitializeGame();
+    }
+
+    private void InitializeGame()
+    {
+        // Initialize game systems here
+        // For example:
+        // LoadPlayerData();
+        // SetupAudio();
+        // InitializeUI();
+    }
+
+    public void StartNewGame()
+    {
+        // Logic for starting a new game
+        //SceneManager.LoadScene("MainGameScene");
+    }
+
+    public void LoadGame()
+    {
+        // Logic for loading a saved game
+    }
+
+    public void SaveGame()
+    {
+        // Logic for saving the game
+    }
+
+    public void QuitGame()
+    {
+        // Logic for quitting the game
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
