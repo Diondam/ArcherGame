05c2740ff33f579072b02c5072a438f39a530ade
diondam <dion.dam.4802@gmail.com>
2024-09-29 10:42:59 +0700

    + IK Spider Anim

--- Final Project Uni/Assets/Scripts/Enemy/IKSpiderAnimation.cs
+++ Final Project Uni/Assets/Scripts/Enemy/IKSpiderAnimation.cs
@@ -5,46 +5,52 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 
 public class IKSpiderAnimation : MonoBehaviour
-{   
-    [FoldoutGroup("Setup")]
-    public Transform IKRigPos;
-    [FoldoutGroup("Setup")]
-    public float maxPositionOffset = 2f; // Maximum offset value for IKRigPos
-    
-    [FoldoutGroup("LegMovement")]
-    public float height = 0.25f;
-    [FoldoutGroup("LegMovement")]
-    public float stepSize = 0.15f;
-    [FoldoutGroup("LegMovement")]
-    public AnimationCurve LegMoveCurve;
-    [FoldoutGroup("LegMovement")]
-    public float totalDuration = 0.2f; // Total duration for the leg movement
-    [FoldoutGroup("LegMovement")]
-    public float velocityMultiplier = 1f; // this broken, lol
-    
-    [FoldoutGroup("SpiderLeg")]
+{
+    [FoldoutGroup("Calculate Setup")]
     public Transform[] legTargets;
-    [FoldoutGroup("SpiderLeg")]
-    public bool[] legMoving;
-    [FoldoutGroup("SpiderLeg")]
-    public bool oddMovedLast = true; // Start with odd legs moving first
+    [FoldoutGroup("Calculate Setup")]
+    public int smoothness = 8;
+    [FoldoutGroup("Calculate Setup")]
+    public float sphereCastRadius = 0.125f;
+    [FoldoutGroup("Calculate Setup")]
+    public float raycastRange = 1.5f;
     
-    int legTargetLength;
+    [FoldoutGroup("Settings")]
+    public bool Calculate = true;
+    [FoldoutGroup("Settings")]
+    public float stepSize = 0.15f;
+    [FoldoutGroup("Settings")]
+    public float velocityMultiplier = 15f;
+    [FoldoutGroup("Settings")]
+    public AnimationCurve LegMoveCurve;
+    [FoldoutGroup("Settings")]
+    public float totalDuration = 0.15f;
+    [FoldoutGroup("Settings")]
+    public float stepHeight = 0.25f;
+    [FoldoutGroup("Settings")]
+    public bool GizmoDebugToggle;
+    
+    
+    [SerializeField] private SpiderIKController _spiderMovement;
+    
     private Vector3[] defaultLegPositions;
     private Vector3[] lastLegPositions;
+    private Vector3 lastBodyUp;
+    private bool[] legMoving;
+    private int nbLegs;
+    
     private Vector3 velocity;
     private Vector3 lastVelocity;
     private Vector3 lastBodyPos;
-    private float raycastRange = 1f;
-    Rigidbody rb;
 
-    static Vector3[] MatchToSurfaceFromAbove(Vector3 point, float halfRange, Vector3 up)
+    Vector3[] MatchToSurfaceFromAbove(Vector3 point, float halfRange, Vector3 up)
     {
         Vector3[] res = new Vector3[2];
+        res[1] = Vector3.zero;
         RaycastHit hit;
-        Ray ray = new Ray(point + halfRange * up, -up);
+        Ray ray = new Ray(point + halfRange * up / 2f, - up);
 
-        if (Physics.Raycast(ray, out hit, 2f * halfRange))
+        if (Physics.SphereCast(ray, sphereCastRadius, out hit, 2f * halfRange))
         {
             res[0] = hit.point;
             res[1] = hit.normal;
@@ -55,118 +61,24 @@ public class IKSpiderAnimation : MonoBehaviour
         }
         return res;
     }
-
-    #region Zic-zac pattern
-
-    bool ToggleOddMoveLast()
-    {
-        for (int i = 0; i < legTargets.Length; ++i)
-        {
-            legMoving[i] = false;
-        }
-        return !oddMovedLast;
-
-    }
-    // Method to check if all odd legs are true
-    public bool allOddLegsTrue()
-    {
-        for (int i = 0; i < legMoving.Length; i += 2)
-        {
-            if (!legMoving[i])
-            {
-                //StartCoroutine(PerformStep(i, defaultLegPositions[i]));
-                return false;
-            }
-        }
-        return true;
-    }
-    // Method to check if all even legs are true
-    public bool allEvenLegsTrue()
-    {
-        for (int i = 1; i < legMoving.Length; i += 2)
-        {
-            if (!legMoving[i])
-            {
-                //StartCoroutine(PerformStep(i, defaultLegPositions[i]));
-                return false;
-            }
-        }
-        return true;
-    }
     
-    #endregion
-
-    void Awake()
+    void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        lastBodyUp = _spiderMovement.GroundSurfaceNormal;
 
-        legTargetLength = legTargets.Length;
-        defaultLegPositions = new Vector3[legTargetLength];
-        lastLegPositions = new Vector3[legTargetLength];
-        legMoving = new bool[legTargetLength];
-
-        for (int i = 0; i < legTargetLength; ++i)
+        nbLegs = legTargets.Length;
+        defaultLegPositions = new Vector3[nbLegs];
+        lastLegPositions = new Vector3[nbLegs];
+        legMoving = new bool[nbLegs];
+        for (int i = 0; i < nbLegs; ++i)
         {
-            defaultLegPositions[i] = legTargets[i].position;
+            defaultLegPositions[i] = legTargets[i].localPosition;
+            lastLegPositions[i] = legTargets[i].position;
             legMoving[i] = false;
-            lastLegPositions[i] = defaultLegPositions[i];
         }
         lastBodyPos = transform.position;
     }
 
-
-
-    void FixedUpdate()
-    {
-        // Calculate Vector3 Velocity of the Spider
-        //velocity = lastBodyPos - transform.position;
-        
-        velocity = lastBodyPos - transform.position;
-        if (velocity.y > 0) velocity.y = -velocity.y;
-        velocity = (velocity + Time.fixedDeltaTime * lastVelocity * velocityMultiplier) / (Time.fixedDeltaTime + 1);
-        
-        if (velocity.magnitude < 0.000025f) velocity = lastVelocity;
-        else lastVelocity = velocity;
-
-        velocity.y = Mathf.Clamp(velocity.y, 0f, float.MaxValue);
-
-        Vector3[] wishPos = new Vector3[legTargets.Length];
-
-        // Variables to keep track of the last moved leg
-        int lastMovedIndex = -1;
-        float maxDistance = stepSize;
-        // Loop through each leg
-        for (int i = 0; i < legTargets.Length; ++i)
-        {   
-            legTargets[i].position = lastLegPositions[i];
-
-            wishPos[i] = Vector3.ProjectOnPlane(defaultLegPositions[i] - (velocity), transform.up);
-            
-            Vector3[] positionAndNormal = MatchToSurfaceFromAbove(wishPos[i], raycastRange, transform.up);
-            defaultLegPositions[i] = positionAndNormal[0];
-
-            // Skip legs that have already moved
-            if (i == lastMovedIndex)
-                continue;
-
-            // Move odd legs first
-            if (oddMovedLast && i % 2 != 0)
-            {   
-                MoveLeg(i, wishPos, ref maxDistance);
-                lastMovedIndex = i;
-            }
-            // Move even legs after odd legs
-            if (!oddMovedLast && i % 2 == 0)
-            {
-                MoveLeg(i, wishPos, ref maxDistance);
-                lastMovedIndex = i;
-            }
-        }
-        
-        lastBodyPos = transform.position;
-    }
-    
-    
     IEnumerator PerformStep(int index, Vector3 targetPoint)
     {
         Vector3 startPos = lastLegPositions[index];
@@ -174,10 +86,10 @@ public class IKSpiderAnimation : MonoBehaviour
         
         float elapsedTime = 0f;
 
-        while (elapsedTime < totalDuration)
+        while (elapsedTime < totalDuration && _spiderMovement.isGrounded)
         {
             float curveTime = elapsedTime / totalDuration;
-            float yOffset = LegMoveCurve.Evaluate(curveTime) * height;
+            float yOffset = LegMoveCurve.Evaluate(curveTime) * stepHeight;
             
             Vector3 nextPos = Vector3.Lerp(startPos, targetPoint, curveTime);
             legTargets[index].position = nextPos + transform.up * yOffset;
@@ -187,7 +99,7 @@ public class IKSpiderAnimation : MonoBehaviour
         }
 
         elapsedTime = 0f;
-        while (elapsedTime < totalDuration)
+        while (elapsedTime < totalDuration && !_spiderMovement.isGrounded)
         {
             float curveTime = elapsedTime / totalDuration;
             Vector3 nextPos = Vector3.Lerp(startPos, targetPoint, curveTime);
@@ -200,41 +112,111 @@ public class IKSpiderAnimation : MonoBehaviour
         legTargets[index].position = targetPoint;
         lastLegPositions[index] = legTargets[index].position;
         legMoving[0] = false;
-        yield return new WaitForSeconds(0.2f); // Introduce a 0.1 second delay
+        //MovementSound.Instance.PLaySoundAt(legTargets[index].position);
     }
-    
-    void MoveLeg(int index, Vector3[] desiredPositions, ref float maxDistance)
+
+    IEnumerator LegsOnAir()
     {
-        float distance = Vector3.Distance(legTargets[index].position, defaultLegPositions[index]);
-        
-        if (distance > maxDistance+maxDistance*10/100)
+        float elapsedTime = 0f;
+        Vector3[] startPositions = new Vector3[nbLegs];
+    
+        for (int i = 0; i < legTargets.Length; i++)
         {
-            maxDistance = distance;
-            StartCoroutine(PerformStep(index, defaultLegPositions[index]));
-            legMoving[index] = true;
-            
+            startPositions[i] = legTargets[i].position;
         }
-        if (allEvenLegsTrue())
+
+        while (elapsedTime < totalDuration)
         {
-            oddMovedLast = ToggleOddMoveLast();
-            //Debug.Log("Even");
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / totalDuration * 1.5f;
+
+            for (int i = 0; i < nbLegs; i++)
+            {
+                legTargets[i].position = Vector3.Lerp(startPositions[i], transform.TransformPoint(defaultLegPositions[i]), t);
+            }
+
+            yield return null;
         }
-        if (allOddLegsTrue())
+
+        // Ensure the legs reach the final default positions
+        for (int i = 0; i < nbLegs; i++)
         {
-            oddMovedLast = ToggleOddMoveLast();
-            //Debug.Log("Odd");
+            legTargets[i].position = transform.TransformPoint(defaultLegPositions[i]);
         }
     }
 
-    private void OnDrawGizmosSelected()
+    void FixedUpdate()
     {
-        if(!this.enabled) return;
-        for (int i = 0; i < legTargetLength; ++i)
+        //velocity = _spiderMovement.transform.position - lastBodyPos;
+        Vector3 currentVelocity = _spiderMovement.rb.velocity;
+        
+        velocity = Vector3.Lerp(lastVelocity, currentVelocity, 1f / (smoothness + 1f));
+
+        if (velocity.magnitude < 0.000025f)
+            velocity = lastVelocity;
+        else
+            lastVelocity = velocity;
+
+        //on Air
+        Calculate = _spiderMovement.isGrounded;
+        if (!Calculate)
+        {
+            //set all target at the position on air here
+            StartCoroutine(LegsOnAir());
+            lastBodyPos = _spiderMovement.transform.position;
+            return;
+        }
+        
+        Vector3[] desiredPositions = new Vector3[nbLegs];
+        int indexToMove = -1;
+        float maxDistance = stepSize;
+        for (int i = 0; i < nbLegs; ++i)
+        {
+            desiredPositions[i] = transform.TransformPoint(defaultLegPositions[i]);
+
+            float distance = Vector3.ProjectOnPlane(desiredPositions[i] + velocity * velocityMultiplier - lastLegPositions[i], -transform.up).magnitude;
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+                indexToMove = i;
+            }
+        }
+        for (int i = 0; i < nbLegs; ++i)
+            if (i != indexToMove)
+                legTargets[i].position = lastLegPositions[i];
+
+        if (indexToMove != -1 && !legMoving[0])
+        {
+            Vector3 targetPoint = desiredPositions[indexToMove] + Mathf.Clamp(velocity.magnitude * velocityMultiplier, 0.0f, 1.5f) * (desiredPositions[indexToMove] - legTargets[indexToMove].position) + velocity * velocityMultiplier;
+
+            // Calculate opposite direction from barrier
+            Vector3 obstacleDirection = (targetPoint - legTargets[indexToMove].position).normalized;
+            Vector3 newTargetPoint = targetPoint + obstacleDirection * (stepSize * 0.6f);
+            
+            Vector3[] positionAndNormalFwd = MatchToSurfaceFromAbove(newTargetPoint + velocity * velocityMultiplier, raycastRange, (_spiderMovement.GroundSurfaceNormal - velocity * 100).normalized);
+            Vector3[] positionAndNormalBwd = MatchToSurfaceFromAbove(newTargetPoint + velocity * velocityMultiplier, raycastRange*(1f + velocity.magnitude), (_spiderMovement.GroundSurfaceNormal + velocity * 75).normalized);
+            
+            legMoving[0] = true;
+            
+            if (positionAndNormalFwd[1] == Vector3.zero)
+                StartCoroutine(PerformStep(indexToMove, positionAndNormalBwd[0]));
+            else
+                StartCoroutine(PerformStep(indexToMove, positionAndNormalFwd[0]));
+        }
+
+        lastBodyPos = _spiderMovement.transform.position;
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!GizmoDebugToggle) return;
+        for (int i = 0; i < nbLegs; ++i)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(defaultLegPositions[i], 0.05f); 
+            Gizmos.DrawWireSphere(legTargets[i].position, 0.05f);
             Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(legTargets[i].position, stepSize);
+            Gizmos.DrawWireSphere(transform.TransformPoint(defaultLegPositions[i]), stepSize);
         }
     }
 }
