e9ded97a8cec097ce47c6981550cf11505aea60a
diondam <dion.dam.4802@gmail.com>
2024-10-09 19:44:08 +0700

    =update

--- Final Project Uni/Assets/Scripts/Player/Skill/Skill_Splitshot.cs
+++ Final Project Uni/Assets/Scripts/Player/Skill/Skill_Splitshot.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class Skill_Splitshot : ISkill
 {
     // Start is called before the first frame update
+    void Start()
+    {
+        Name = "Split shot";
+        type = SkillType.PASSIVE;
+        _pc.GetComponent<ArrowController>().IsSplitShot = true;
+    }
     void SetToggle(bool toggle)
     {
         Name = "Split shot";
