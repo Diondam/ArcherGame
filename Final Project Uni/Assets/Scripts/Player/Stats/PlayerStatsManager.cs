6b629a378e486b853a46f8b017aec33f0d0220a6
diondam <dion.dam.4802@gmail.com>
2024-10-20 23:04:55 +0700

    + dam: add Stat Save Permanent System

---
+++ Final Project Uni/Assets/Scripts/Player/Stats/PlayerStatsManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace PA
+{
+    public class PlayerStatsManager : MonoBehaviour
+    {
+        public PlayerStats playerStats;
+        public StatsUI statsUI;
+        public int playerGold = 1000; // Giả lập lượng vàng
+
+        private void Start()
+        {
+            //delay 1 frame to get player stats
+            StartCoroutine(DelayedStart());
+        }
+
+        private IEnumerator DelayedStart()
+        {
+            yield return null;
+            playerStats = GameObject.Find("Player").GetComponent<PlayerStats>();
+            PermanentStats.LoadStats();
+            UpdatePlayerStats();
+            UpdateUI();
+        }
+
+        //update to display for player base on data from total value when play
+        //value display will be Round to Int mean different from actual value
+        public void UpdatePlayerStats()
+        {
+            //subtract because after upgrade, the value is more than 1, end we only need increase percentage amount
+            playerStats.bonusSpeed = playerStats.defaultSpeed * (PermanentStats.Speed - 1f);
+            playerStats.bonusDamage = Mathf.CeilToInt(playerStats.defaultDamage * (PermanentStats.Damage - 1f));
+            print("bonusDamage: " + playerStats.bonusDamage);
+            playerStats.HealthFromPermanent = Mathf.CeilToInt(playerStats.playerHealth * (PermanentStats.HP -1f)); // 10 * (1.02-1)
+            playerStats.UpdateBonusValue();
+        }
+
+        public void UpdateUI()
+        {
+            // use data from total value
+            statsUI.UpdateStatsDisplay(
+                playerStats.playerHealth + playerStats.HealthFromPermanent,
+                playerStats.speed,
+                playerStats.Damage
+            );
+        }
+
+        //update actual value 
+        public void UpgradeStats(float percentage)
+        {
+            int cost = percentage == 0.02f ? 100 : 200; // 100 gold for 2%, 200 gold for 4%
+            if (playerGold >= cost)
+            {
+                playerGold -= cost;
+                PermanentStats.UpdateStat("HP", PermanentStats.HP + percentage);
+                PermanentStats.UpdateStat("Speed", PermanentStats.Speed + percentage);
+                PermanentStats.UpdateStat("Damage", PermanentStats.Damage + percentage);
+                UpdatePlayerStats();
+                UpdateUI();
+                statsUI.UpdateGoldDisplay(playerGold);
+            }
+            else
+            {
+                Debug.Log("Not enough gold!");
+            }
+        }
+
+        public void ConfirmStats()
+        {
+            PermanentStats.SaveStats();
+            Debug.Log(Application.dataPath + "/player_stats.json");
+            Debug.Log("Stats saved permanently!");
+        }
+    }
+}
