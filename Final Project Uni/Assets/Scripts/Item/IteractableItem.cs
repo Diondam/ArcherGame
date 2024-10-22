e79f04d4f54cb79520d1b356652e1a99087b159b
diondam <dion.dam.4802@gmail.com>
2024-10-22 22:00:38 +0700

    + dam : add knowledge level + Buff Item

--- Final Project Uni/Assets/Scripts/Item/IteractableItem.cs
+++ Final Project Uni/Assets/Scripts/Item/IteractableItem.cs
@@ -9,6 +9,7 @@ public class IteractableItem : MonoBehaviour
 {
     public GameObject dialog;
     public Button activeButton;
+    public int requireKnowledgeLevel;
 
     [SerializeField]
     //only need active dialog one time
@@ -17,7 +18,6 @@ public class IteractableItem : MonoBehaviour
     public UnityEvent OnTouch;
 
     private ScaleEffect scaleEffect;
-     
 
     private void Awake()
     {
@@ -34,7 +34,6 @@ public class IteractableItem : MonoBehaviour
     {
         isOpen = true;
     }
-
 
     private void Start()
     {
@@ -92,7 +91,8 @@ public class IteractableItem : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if(isOpen == true) return;
+            if (isOpen == true)
+                return;
             activeButton.gameObject.SetActive(true);
         }
     }
