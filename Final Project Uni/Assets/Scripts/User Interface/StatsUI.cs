41f97fe2e3ac921b49c691a6bce1f9dc6f03b607
diondam <dion.dam.4802@gmail.com>
2024-10-30 18:24:26 +0700

    + dam : add save gold function , update UI perma shop

--- Final Project Uni/Assets/Scripts/User Interface/StatsUI.cs
+++ Final Project Uni/Assets/Scripts/User Interface/StatsUI.cs
@@ -10,7 +10,7 @@ public class StatsUI : MonoBehaviour
     public Button confirmModifierPermanentButton;
 
     #region HP Buttons
-
+    [Header("HP")]
     public TMP_Text healthStat;
     public Button hpPlusButton;
     public Button hpMinusButton;
@@ -18,7 +18,7 @@ public class StatsUI : MonoBehaviour
     #endregion
 
     #region Speed Buttons
-
+    [Header("Speed")]
     public TMP_Text speedStat;
     public Button speedPlusButton;
     public Button speedMinusButton;
@@ -26,17 +26,19 @@ public class StatsUI : MonoBehaviour
     #endregion
 
     #region Damage Buttons
-
+    [Header("Damage")]
     public TMP_Text damageStat;
     public Button damagePlusButton;
     public Button damageMinusButton;
 
     #endregion
 
-    public PlayerStats playerStats; 
+    public PlayerStats playerStats;
 
     private void Start()
     {
+        playerStats = GameObject.FindWithTag("Player").GetComponent<PlayerStats>();
+
         hpPlusButton.onClick.AddListener(() => playerStats.ModifyStat("HP", true));
         hpMinusButton.onClick.AddListener(() => playerStats.ModifyStat("HP", false));
         speedPlusButton.onClick.AddListener(() => playerStats.ModifyStat("Speed", true));
@@ -60,6 +62,12 @@ public class StatsUI : MonoBehaviour
         );
     }
 
+    private void OnEnable()
+    {
+        StartCoroutine(DelayedStart());
+        Debug.Log("StatsUI OnEnable");
+    }
+
     public void UpdateStatsDisplay(int health, float speed, int damage, int gold)
     {
         healthStat.text = $"Health: {health}";
@@ -70,6 +78,9 @@ public class StatsUI : MonoBehaviour
 
     private void CloseStatsUI()
     {
+        //cancel changes
+        playerStats.CancelChanges();
+        //turn off the UI
         gameObject.SetActive(false);
     }
 }
