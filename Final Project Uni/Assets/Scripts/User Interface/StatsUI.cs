8dbe1a104a53e29b8b02f3792ae7ae38bea00216
diondam <dion.dam.4802@gmail.com>
2024-11-06 16:58:30 +0700

    dam - Update Stat UI

--- Final Project Uni/Assets/Scripts/User Interface/StatsUI.cs
+++ Final Project Uni/Assets/Scripts/User Interface/StatsUI.cs
@@ -1,6 +1,7 @@
 using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,6 +14,8 @@ public class StatsUI : MonoBehaviour
     SkillHolder skillHolder;
     public List<GameObject> skillList = new List<GameObject>();
     public List<Image> skillHolderObj;
+    private float baseHealth, bonusHealth, baseSpeed, bonusSpeed, baseStamina, bonusStamina, baseStaRegen, bonusStaRegen, baseAtk, bonusATK;
+    public TMP_Text baseHealthText, bonusHealthText, baseSpeedText, bonusSpeedText, baseStaminaText, bonusStaminaText, baseStaRegenText, bonusStaRegenText, baseAtkText, bonusATKText;
     // Start is called before the first frame update
     [Button]
     public void UpdateInfo()
@@ -24,6 +27,32 @@ public class StatsUI : MonoBehaviour
         {
             skillHolderObj[i].sprite = skillList[i].GetComponent<ISkill>().Icon;
         }
+        ShowStat();
+    }
+
+    public void ShowStat()
+    {
+        baseHealth = (playerStat.totalMaxHealth - playerStat.bonusHealth);
+        bonusHealth = (playerStat.bonusHealth);
+        baseSpeed = playerStat.speed - playerStat.bonusSpeed;
+        bonusSpeed = playerStat.bonusSpeed;
+        baseStamina = (int)(playerStat.maxStamina - playerStat.bonusMaxStamina);
+        bonusStamina = (playerStat.bonusMaxStamina);
+        baseStaRegen = (playerStat.regenRate - playerStat.bonusRegenRate);
+        bonusStaRegen = (playerStat.bonusRegenRate);
+        baseAtk = (playerStat.defaultDamage * playerStat.PermaDamage_Percent);
+        bonusATK = (playerStat.bonusDamage);
+
+        baseHealthText.text = "" + baseHealth.ToString("0.0"); 
+        bonusHealthText.text = bonusHealth > 0 ? "+" + bonusHealth.ToString("0.0") : ""; 
+        baseSpeedText.text = "" + baseSpeed.ToString("0.0"); 
+        bonusSpeedText.text = bonusSpeed > 0 ? "+" + bonusSpeed.ToString("0.0") : "";
+        baseStaminaText.text = "" + baseStamina;
+        bonusStaminaText.text = bonusStamina > 0 ? "+" + bonusStamina.ToString("0.0") : ""; 
+        baseStaRegenText.text = "" + baseStaRegen.ToString("0.0"); 
+        bonusStaRegenText.text = bonusStaRegen > 0 ? "+" + bonusStaRegen.ToString("0.0") : ""; 
+        baseAtkText.text = "" + baseAtk.ToString("0.0"); 
+        bonusATKText.text = bonusATK > 0 ? "+" + bonusATK.ToString("0.0") : "";
     }
 
 }
